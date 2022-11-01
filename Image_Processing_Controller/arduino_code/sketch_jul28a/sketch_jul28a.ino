54a3b0c49071f7f68da12ac588ed1df402ac2634
Vitharana <vitharana1996@gmail.com>
2022-11-01 09:26:51 +0900

    Add files via upload

---
+++ Image_Processing_Controller/arduino_code/sketch_jul28a/sketch_jul28a.ino
@@ -0,0 +1,42 @@
+int x;
+
+int out[5] = {8,9,10,11,12};
+//left,up,right,down,release
+bool state = true;
+void setup() {
+
+ for(int i =0;i<5;i++)
+  pinMode(out[i],OUTPUT);
+  
+ Serial.begin(115200);
+ Serial.setTimeout(1);
+
+ pinMode(13,OUTPUT);
+}
+void loop() {
+ while (!Serial.available());
+ x = Serial.readString().toInt();
+ Serial.print(x);
+
+ if(x ==5){
+  digitalWrite(13,state);
+  //delay(10);
+  state = !state;
+ }
+
+ output(x);
+}
+
+
+
+void output(int i){
+
+for(int k = 0;k<5;k++){
+  if(k == i)
+    digitalWrite(out[k],HIGH);
+
+  else
+    digitalWrite(out[k],LOW);
+
+}
+}
